5cd4de0d0968d54246fc5c800224b365138cb499
saipranayteja <pranayteja.ch@algonox.com>
2023-04-13 15:36:04 +0530

    okay

---
+++ jenkinsfile-practise-declaritive.groovy
@@ -0,0 +1,37 @@
+pipeline {
+    agent any
+
+
+parameters{
+string(name: "NAME", defaultValue: "Go", description: "Tf you gonna do?")
+}
+
+  environment {
+        NAME = input message: 'Please enter the Name',parameters: [string(defaultValue: '',description: '',name: 'NAME')]
+        course    = input message: 'Please enter the Course Name',parameters: [string(defaultValue: '',description: '',name: 'CourseName')]
+    }
+
+stages {
+    stage("first") {
+        steps{
+        echo "Name is ${NAME}"
+
+        }
+    }
+    stage("second") {
+        steps{
+        echo "course is ${course}"
+        sh 'env'
+        }
+        
+    }
+}
+
+
+post {
+    always {
+        echo "stupid dumbfuck"
+    }
+}
+
+}
