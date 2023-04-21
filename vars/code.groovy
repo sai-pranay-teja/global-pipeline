e1699966f62f911494f0c8e9aaefa612fb8c1d60
saipranayteja <pranayteja.ch@algonox.com>
2023-04-21 19:52:24 +0530

    fo

--- vars/code.groovy
+++ vars/code.groovy
@@ -1,11 +1,11 @@
 def case_1() {
     if(software=="nginx")
-      sh 'sudo install nginx -y'
+      sh 'yum install nginx -y'
 
 }
 
 def case_2() {
     if(software=="git")
-      sh 'sudo install git -y'
+      sh 'yum install git -y'
     
 }
