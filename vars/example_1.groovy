0396476e21d598910094fbbbe75772aaf0c6c907
saipranayteja <pranayteja.ch@algonox.com>
2023-04-21 18:27:47 +0530

    gp

--- vars/example_1.groovy
+++ vars/example_1.groovy
@@ -8,15 +8,12 @@ parameters{
 string(name: "NAME", defaultValue: "Go", description: "Tf you gonna do?")
 }
 
-  environment {
-
-        NAME = input message: 'Please enter the Name',parameters: [string(defaultValue: '',description: '',name: 'NAME')]
-        course    = input message: 'Please enter the Course Name',parameters: [string(defaultValue: '',description: '',name: 'CourseName')]
-    }
 
 stages {
     
     stage("first") {
+        NAME = input message: 'Please enter the Name',parameters: [string(defaultValue: '',description: '',name: 'NAME')]
+
         when {
             expression { params.NAME == 'Bro' }
         }
@@ -26,6 +23,8 @@ stages {
         }
     }
     stage("second") {
+        course    = input message: 'Please enter the Course Name',parameters: [string(defaultValue: '',description: '',name: 'CourseName')]
+
         when {
             expression { params.NAME == 'COOL' }
         }
