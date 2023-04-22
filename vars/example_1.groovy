81bcbc3056d1baf1a9f47940d0a4bb234d78f11b
saipranayteja <pranayteja.ch@algonox.com>
2023-04-22 11:43:10 +0530

    go

--- vars/example_1.groovy
+++ vars/example_1.groovy
@@ -3,28 +3,38 @@ def call() {
 pipeline {
     agent any
     stages {
-        stage('first') {
-            steps {
-                script {
-                    if(software=="npm")
-                      sh 'echo npm > npm.txt'
-                    // code.case_1()
-                }
-        }
-        }
-        stage('second') {
-            steps{
-                script{
-                    if(software=="mvn")
-                      sh 'mvn package'
-                    // code.case_2()
+        // stage('first') {
+        //     steps {
+        //         script {
+        //             if(software=="npm")
+        //               sh 'echo npm > npm.txt'
+        //             // code.case_1()
+        //         }
+        // }
+        // }
+        // stage('second') {
+        //     steps{
+        //         script{
+        //             if(software=="mvn")
+        //               sh 'mvn package'
+        //             // code.case_2()
 
-                }
+        //         }
+        //     }
+                
+            
+        //     }
+
+           stage('Email') {
+            steps{
+                mail bcc: '', body: '', cc: '', from: '', replyTo: '', subject: 'Demo Purpose', to: 'algonox1.1@gmail.com'
             }
                 
             
             }
         }
+
+
     }
 }
 
