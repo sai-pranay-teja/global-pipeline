dfb86abb1917204b135849197778732b91e937a6
saipranayteja <pranayteja.ch@algonox.com>
2023-05-05 01:04:33 +0530

    szxdc

--- vars/example_1.groovy
+++ vars/example_1.groovy
@@ -20,9 +20,7 @@ pipeline {
                     return gitTag;
                 }
             }
-            steps{
-                
-            }
+            
                 
             
             }
